ef5363c03e03d23120448bf196952a8800e0218f
A Student <student@nowhere.edu>
2025-10-19 21:05:41 +0000

    Oct 19, 2025, 2:05 PM

---
+++ tests.cpp
@@ -0,0 +1,59 @@
+//----------------------------------------------------------
+// CS161 Assignment Starter Code
+// Copyright Andrew Scholer (ascholer@chemeketa.edu)
+// Neither this code, nor any works derived from it
+//    may be republished without approval.
+//----------------------------------------------------------
+
+//Bring in unit testing code and tell it to build a main function
+//If you have multiple unit test files, only one should have this define
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+
+//Use Approx from doctest without saying doctest::Approx
+using doctest::Approx;
+
+//-----------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+//Your functions here
+
+
+
+///----------------------------------------------------------------------------------
+/// Tests
+/// Uncomment tests to work on them. Make sure any test that does not compile or
+/// causes a crash gets commented back out. Any test that runs should be left
+/// on (uncommented), even if the test fails.
+///----------------------------------------------------------------------------------
+
+//TEST_CASE( "getArea" ) {
+//    cout << "1: getArea" << endl;
+//    CHECK( getArea(2, 4) == 8 );
+//    CHECK( getArea(12, 7) == 84 );
+//}
+
+//TEST_CASE( "getVolume" ) {
+//    cout << "2: getVolume" << endl;
+//    CHECK( getVolume(2, 4, 3) == 24 );
+//    CHECK( getVolume(12, 7, 2) == 168 );
+//}
+
+//TEST_CASE( "leftoverCardboard" ) {
+//    cout << "3: leftoverCardboard" << endl;
+//    CHECK( leftoverCardboard(3) == 36 );
+//    CHECK( leftoverCardboard(10) == 400 );
+//}
+
+//TEST_CASE( "getMaxHeight" ) {
+//    cout << "4: getMaxHeight" << endl;
+//    CHECK( getMaxHeight(5, 10) == 2 );
+//    CHECK( getMaxHeight(10, 5) == 2 );
+//    CHECK( getMaxHeight(12, 16) == 5 );
+//    CHECK( getMaxHeight(18, 16) == 7 );
+//}
+
+//Add your own test case for bowLength here
